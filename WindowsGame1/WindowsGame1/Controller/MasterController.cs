22d50057cf4c066eb653285f5c4f2f690d9169c8
Hampus Johansson <hamjo788@student.liu.se>
2012-11-27 10:51:10 +0100

    Initial commit of base classes

---
+++ WindowsGame1/WindowsGame1/Controller/MasterController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1.Controller
+{
+    public class MasterController : Microsoft.Xna.Framework.Game
+    {
+
+    }
+}
